2faf7b9c3ba0bae4b05f2a0f4947953c99b2e75f
Nahal Bagheri <91636662+nbagheri3@users.noreply.github.com>
2025-01-18 12:40:09 -0800

    Add files via upload
    A clean and well-structured Mortgage Calculator implemented in Java.

---
+++ Mortgage_Calculator.java
@@ -0,0 +1,45 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Mortgage_Calculator {
+    public static void main(String[] args) {
+
+       int principal = (int) readNumber("Principal ($1K - $1M):", 1000, 1_000_000);
+       float annualInterest = (float) readNumber("Annual Interest Rate:", 1, 30);
+       byte years = (byte) readNumber("Period (Years):", 1, 30);
+
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+
+        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.print("Mortgage: " + formattedMortgage);
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while(true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " +  max);
+        }
+        return value;
+    }
+
+    public static double calculateMortgage(int principal,
+                                          float annualInterest,
+                                          byte years) {
+        final byte MONTH_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        float monthlyInterest = (annualInterest / PERCENT) / MONTH_IN_YEAR;
+        short numberOfPayments = (short) (years * MONTH_IN_YEAR);
+
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))/
+                (Math.pow(1+monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    }
+
+}
